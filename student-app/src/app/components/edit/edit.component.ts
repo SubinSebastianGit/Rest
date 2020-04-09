00748bb359ba21c6da480338d74a8af874494a22
SubinSebastianGit <subinsebastian.sf@gmail.com>
2020-04-09 21:38:52 +0530

    fifth commit

--- student-app/src/app/components/edit/edit.component.ts
+++ student-app/src/app/components/edit/edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+
+import { List } from '../../list.model';
 import { ListService } from '../../list.service';
 
 @Component({
@@ -6,11 +10,48 @@ import { ListService } from '../../list.service';
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent implements OnInit {
+export class EditComponent {
 
-  constructor(private listservice: ListService) { }
+  list: List;
+
+  enteredTitle = '';
+  enteredrollno = '';
+  title = '';
+  rollno = '';
+
+  id = this.route.snapshot.paramMap.get('id');
+
+  constructor(private route: ActivatedRoute, private listservice: ListService, private router: Router) { 
+
+  }
 
   ngOnInit(): void {
+  this.fetchListDetail();
+  }
+
+  fetchListDetail() {
+    
+    this.listservice.getListById(this.id).subscribe((data: List) => {
+      this.list = data;
+      console.log('Data Requested...');
+      console.log(this.list);
+    });
+
+  }
+
+  onUpdateData() {
+  this.title = '';
+  this.rollno = '';
+  this.updateList();
+  }
+
+  updateList() {
+    
+    this.listservice.updateList(this.id, this.list.title, this.list.rollno).subscribe(() => {
+      console.log('List Updated...');
+    });
+    alert('Data Updated!');
+
   }
 
 }
